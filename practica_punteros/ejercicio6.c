8ea9928ba434f70899c2dfda2a33eb1773f25180
Maria Claudia Perez Escalante <mariaclaudiaperezes@gmail.com>
2020-09-27 01:19:33 -0300

    se agrego la funcion strDer

--- practica_punteros/ejercicio6.c
+++ practica_punteros/ejercicio6.c
@@ -12,7 +12,8 @@ void strCopia(char *destino, const char *origen); // Copiador
 // K&R el comportamiento es indefinido)*/
 char* reverse(char *string);//retorna una cadena que es string invertida
 void strIzq(char *destino, const char *origen); // Saca blancos Izq.
-// void strDer(char *destino, const char *origen); // Saca blancos Der.
+void strDer(char *destino, const char *origen); // Saca blancos Der.
+void strCopialen(char *destino, const char *origen, int length);
 // void strAmbos(char *destino, const char *origen); // Saca blancos Izq. y Der.
 // void strMayMin(char *destino, const char *origen, may_min m); // Convierte May. Min.
 
@@ -22,10 +23,10 @@ int main(){
   int largo = strLargo(text1)+1;
   char *result = (char *)malloc (largo);
   char* reves;
-  // if(result == NULL)
-  // return -1;//sino pudo reservar memoria para result
-  // printf("La cadena: ");
-  // puts(text1);
+  if(result == NULL)
+    return -1;//sino pudo reservar memoria para result
+  printf("La cadena: ");
+  puts(text1);
   printf("Se encuentra: %s\n",(strVacio(text1) ? "No vacia" : "Vacia"));
   printf("Largo : %d\n", strLargo(text1));
   strCopia(result,text1);
@@ -33,9 +34,9 @@ int main(){
   strIzq(result,text1);
   printf("Sin blancos a la Izq: [%s]\n", result);
   // puts(result);
-  // strDer(result,text1);
-  // printf("Der : [%s]\n", result);
-  // strAmbos(result,text1);
+  strDer(result,text1);
+  printf("Der : [%s]\n", result);
+  //strAmbos(result,text1);
   // printf("Ambos: [%s], sin blancos al principio ni al final.\n", result);
   // strMayMin(result,text1, MAYUSCULAS);
   // printf("Mayusculas : [%s]\n", result);
@@ -71,7 +72,17 @@ void strCopia(char *destino, const char *origen){
   for (i = 0; origen[i] != 0; i++){
     destino[i] = origen[i];
   }
-   destino[i+1]= 0;
+   destino[i]= 0;
+}
+
+void strCopialen(char *destino, const char *origen, int length){
+  int i;
+  for (i = 0; i < length; i++){
+    destino[i] = origen[i];
+  }
+
+  destino[i] = '\0';
+
 }
 
 char* reverse(char *string){
@@ -92,5 +103,27 @@ void strIzq(char *destino, const char *origen){
   }
 
   strCopia(destino, &origen[indice]);
+
+}
+
+void strDer(char *destino, const char *origen){
+  int i = 0;
+  int espacio = 0;
+  int lenght;
+
+  while(origen[i] != 0){
+    if(origen[i] == 32){
+      espacio++;
+    }else{
+      espacio = 0;
+    }
+    i++;
+  }
+  
+  lenght = strLargo(origen);
+
+  lenght -= espacio ;
+
+  strCopialen(destino, origen, lenght);
 
 }
